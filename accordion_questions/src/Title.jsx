f9d9214842bc7535c37b33b07aff3732303d3faa
athabarokahfc <athabarokahfc@gmail.com>
2024-08-03 13:04:17 +0000

    menu filter assignment

--- accordion_questions/src/Title.jsx
+++ accordion_questions/src/Title.jsx
@@ -1,5 +1,5 @@
 const Title = () => {
-    return <h1>React Course - Project</h1> ; 
+    return <h1>Title</h1> ; 
 }
 
 export default Title;
--- menu_filter/src/App.jsx
+++ menu_filter/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Title from "./Title.jsx";
 import menu from "./data.js";
 import Menu from "./Menu.jsx";
+import Categories from "./Categories.jsx";
 
 
 const allCategories = ['all', ...new Set(menu.map(item => item.category) )]
@@ -9,13 +10,23 @@ const allCategories = ['all', ...new Set(menu.map(item => item.category) )]
 const App = () => {
   const [menuItems, setMenuItem] = useState(menu);
   const [categories, setCategories] = useState(allCategories);
-  
+
+  const filterItems = (category) => {
+    if(category === 'all') {
+      setMenuItem(menu); 
+      return;
+    }
+    console.log(category);
+    const newItems = menu.filter((item) => item.category === category);
+    setMenuItem(newItems);
+  }  
   return (
     <main>
       <section className="menu">
         <Title title={'Our Menu'}/>
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
-      </section>
+      </section>     
     </main>
   );
 };
